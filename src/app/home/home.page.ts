fd5f86fa5b546664951484ea2c67dc96ced4583a
Kevin Wong <kevin@kevinwong.co>
2020-02-23 14:50:57 -0500

    Testing TTS

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  text_sentences = [];
+ 
+  constructor(
+    private tts: TextToSpeech
+  ) {
+    this.text_sentences = [
+      "There is no such thing as fun for the whole family",
+      "If you can't have fun, there's no sense in doing it.",
+      "Stand up for what is right, regardless of who is committing the wrong."
+    ]
+  }
 
+  textToSpeech(text) {
+    this.tts.speak(text)
+      .then(() => console.log('Success'))
+      .catch((reason: any) => console.log(reason));
+  }
 }
