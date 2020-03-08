1bb9f5540ffd78aac7a9ebcacda08b9dfa48a32a
Kevin Wong <kevin@kevinwong.co>
2020-03-08 13:38:02 -0400

    Added environments variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCUW3yp6wvY_ilG5DZFXm_xI4PILKnnxeU",
+        authDomain: "visualspeech-ac981.firebaseapp.com",
+        databaseURL: "https://visualspeech-ac981.firebaseio.com",
+        projectId: "visualspeech-ac981",
+        storageBucket: "visualspeech-ac981.appspot.com",
+        messagingSenderId: "364766529614",
+        appId: "1:364766529614:web:df1bfde55af16bac219b1f",
+        measurementId: "G-ZKVVFP4FBR"
+    }
+  };
